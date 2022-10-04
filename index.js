ec51a98e7b5f5dfdc32d95138484e75efa013227
mIsmail-dev <muhammad.ismail@ocloudsolutions.net>
2022-10-04 20:14:10 +0500

    Added /api/rentals Endpoint...

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const genreRoutes = require('./routes/genreRoutes')
 const customerRoutes = require('./routes/customerRoutes')
 const movieRoutes = require('./routes/movieRoutes')
+const rentalRoutes = require('./routes/rentalRoutes')
 
 const app = express()
 
@@ -16,6 +17,7 @@ app.use(express.json())
 app.use('/api/genres', genreRoutes)
 app.use('/api/customers', customerRoutes)
 app.use('/api/movies', movieRoutes)
+app.use('/api/rentals', rentalRoutes);
 
 app.get('/', (req, res) => {
     res.send('App is running...')
@@ -23,3 +25,11 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Listening at Port ${port}...`))
+
+
+// - Transaction
+// it is possible that one async operation doesn't Complete like rental or MongoDb Connection Drops. then the second Async Operation will not be complete
+// That's where we need TRANSACTION operation. Transaction makes sure that both Async Operation Completes (means both will update the state or none of this will apply) or Both not. So, They are Atomic. They both completes or both roll back.
+// It is not present in Non-Relation database, Only in Relational Database.
+// They are technique that is Two phase Commits through which we can implement Transaction but it is - Advanced Topic
+// NPM Package that will simulate Transaction in Mongoose.
---
+++ routes/rentalRoutes.js
@@ -0,0 +1,63 @@
+const {Rental, validate} = require('../models/rentalModel'); 
+const {Movie} = require('../models/movieModel'); 
+const {Customer} = require('../models/cutomerModel');
+const Fawn = require('fawn')
+const express = require('express');
+const mongoose = require('mongoose')
+const router = express.Router();
+
+// Fawn.init(mongoose)
+
+// Transaction
+// A group of operations that should be performed as a unit. So, either all these operation will complete and change the state of the database or if any of the operation fails, we will roll back and database will go back to initial state.
+// In MongoDb, we don't have Transaction.
+// There is a technique that is Two phase Commits through which we can implement Transaction but it is - Advanced Topic
+// NPM Package Fawn that will simulate Transaction in Mongoose.
+
+router.get('/', async (req, res) => {
+  const rentals = await Rental.find().sort('-dateOut');
+  res.send(rentals);
+});
+
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const customer = await Customer.findById(req.body.customerId);
+  if (!customer) return res.status(400).send('Invalid customer.');
+
+  const movie = await Movie.findById(req.body.movieId);
+  if (!movie) return res.status(400).send('Invalid movie.');
+
+  if (movie.numberInStock === 0) return res.status(400).send('Movie not in stock.');
+
+  let rental = new Rental({ 
+    customer: {
+      _id: customer._id,
+      name: customer.name, 
+      phone: customer.phone
+    },
+    movie: {
+      _id: movie._id,
+      title: movie.title,
+      dailyRentalRate: movie.dailyRentalRate
+    }
+  });
+
+  rental = await rental.save();
+
+  movie.numberInStock--;
+  movie.save();
+  res.send(rental);
+  
+});
+
+router.get('/:id', async (req, res) => {
+  const rental = await Rental.findById(req.params.id);
+
+  if (!rental) return res.status(404).send('The rental with the given ID was not found.');
+
+  res.send(rental);
+});
+
+module.exports = router; 
